4b5b768e5c096a91b64ca3565d3b71840ea5f3de
mirul22 <amirulikmal.biz@gmail.com>
2019-03-10 00:09:36 +0800

    technical skills, corousel custom typed tweek and window on scroll

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
   var typed = new Typed(".typed", {
     strings: [
-      "Software Enginner.",
+      "Software Engineer.",
       "Web Developer.",
       "Software Developer.",
       "Student."
@@ -16,5 +16,58 @@ $(document).ready(function() {
     loop: true,
     startDelay: 1000,
     showCursor: false
+  });
+
+  var nameTopOffset = $("#myName").offset().top;
+  var setName = 0;
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      938: {
+        items: 4
+      }
+    }
+  });
+
+  var skillsTopOffset = $(".skillsSection").offset().top;
+
+  $(window).scroll(function() {
+    if (window.pageYOffset > nameTopOffset - $(window).height() + 100) {
+      if (setName === 0) {
+        var typed2 = new Typed("#myName", {
+          strings: ["Amirul Ikmal"],
+          typeSpeed: 80
+        });
+        setName = 1;
+      }
+    }
+
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: "#c0392b",
+        traceColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+          $(this.el)
+            .find(".percent")
+            .text(Math.round(percent));
+        }
+      });
+    }
   });
 });
